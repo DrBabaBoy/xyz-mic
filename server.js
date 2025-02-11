f51b13425a0df57312fd734a2584586dd3bc7f07
userxboy <88602948+DrBabaBoy@users.noreply.github.com>
2025-02-11 12:57:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Sirve los archivos estáticos desde la carpeta public
 app.use(express.static('public'));
 
-// Manejo de conexiones Socket.IO
 io.on('connection', (socket) => {
     console.log('Un cliente se ha conectado');
 
     socket.on('audio', (data) => {
-        // Reenvía el audio recibido a todos los demás clientes
         socket.broadcast.emit('audio', data);
     });
 
@@ -23,7 +20,6 @@ io.on('connection', (socket) => {
     });
 });
 
-// Inicia el servidor en el puerto 3000
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
